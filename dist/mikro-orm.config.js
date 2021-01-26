bc65b4abc5681c9c3b34e7e65a72ad2e6e21ce27
jonathan-dwight <jd.buendia0711@gmail.com>
2021-01-25 19:03:33 -0800

    Create setup for backend using typescript/node express graphql apollo server postgreSQL and mikro-orm

---
+++ dist/mikro-orm.config.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Post_1 = require("./entities/Post");
+const path_1 = __importDefault(require("path"));
+exports.default = {
+    migrations: {
+        path: path_1.default.join(__dirname, "./migrations"),
+        pattern: /^[\w-]+\d+\.[tj]s$/,
+    },
+    entities: [Post_1.Post],
+    dbName: "reddit-clone",
+    type: "postgresql",
+    debug: process.env.NODE_ENV !== "production",
+};
+//# sourceMappingURL=mikro-orm.config.js.map
