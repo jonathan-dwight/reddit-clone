8f2a2e0aed148a2c3370f1315842c8952ac91af9
jonathan-dwight <jd.buendia0711@gmail.com>
2021-02-02 20:52:33 -0800

    Add Login Resolver

--- dist/resolvers/user.js
+++ dist/resolvers/user.js
@@ -41,27 +41,123 @@ __decorate([
 UsernamePasswordInput = __decorate([
     type_graphql_1.InputType()
 ], UsernamePasswordInput);
+let FieldError = class FieldError {
+};
+__decorate([
+    type_graphql_1.Field(),
+    __metadata("design:type", String)
+], FieldError.prototype, "field", void 0);
+__decorate([
+    type_graphql_1.Field(),
+    __metadata("design:type", String)
+], FieldError.prototype, "message", void 0);
+FieldError = __decorate([
+    type_graphql_1.ObjectType()
+], FieldError);
+let UserResponse = class UserResponse {
+};
+__decorate([
+    type_graphql_1.Field(() => [FieldError], { nullable: true }),
+    __metadata("design:type", Array)
+], UserResponse.prototype, "errors", void 0);
+__decorate([
+    type_graphql_1.Field(() => User_1.User, { nullable: true }),
+    __metadata("design:type", User_1.User)
+], UserResponse.prototype, "user", void 0);
+UserResponse = __decorate([
+    type_graphql_1.ObjectType()
+], UserResponse);
 let UserResolver = class UserResolver {
     register(options, { em }) {
         return __awaiter(this, void 0, void 0, function* () {
+            if (options.username.length <= 2) {
+                return {
+                    errors: [{
+                            field: "username",
+                            message: "length must be greater than 2"
+                        }]
+                };
+            }
+            if (options.username.length <= 2) {
+                return {
+                    errors: [
+                        {
+                            field: "password",
+                            message: "length must be greater than 2",
+                        },
+                    ],
+                };
+            }
             const hashPassword = yield argon2_1.default.hash(options.password);
             const user = em.create(User_1.User, {
                 username: options.username,
-                password: hashPassword
+                password: hashPassword,
             });
-            yield em.persistAndFlush(user);
-            return user;
+            try {
+                yield em.persistAndFlush(user);
+            }
+            catch (error) {
+                if (error.code === "23505" || error.detail.includes('already exists')) {
+                    return {
+                        errors: [{
+                                field: 'username',
+                                message: 'username already taken'
+                            }]
+                    };
+                }
+                console.log('message: ', error.message);
+            }
+            return {
+                user,
+            };
+        });
+    }
+    login(options, { em }) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const user = yield em.findOne(User_1.User, { username: options.username });
+            if (!user) {
+                return {
+                    errors: [
+                        {
+                            field: "username",
+                            message: "username doesn't exist"
+                        },
+                    ]
+                };
+            }
+            const valid = yield argon2_1.default.verify(user.password, options.password);
+            if (!valid) {
+                return {
+                    errors: [
+                        {
+                            field: "password",
+                            message: "incorrect password"
+                        },
+                    ]
+                };
+            }
+            return {
+                user,
+            };
         });
     }
 };
 __decorate([
-    type_graphql_1.Mutation(() => User_1.User),
-    __param(0, type_graphql_1.Arg('options', () => UsernamePasswordInput)),
+    type_graphql_1.Mutation(() => UserResponse),
+    __param(0, type_graphql_1.Arg("options", () => UsernamePasswordInput)),
     __param(1, type_graphql_1.Ctx()),
     __metadata("design:type", Function),
     __metadata("design:paramtypes", [UsernamePasswordInput, Object]),
     __metadata("design:returntype", Promise)
 ], UserResolver.prototype, "register", null);
+__decorate([
+    type_graphql_1.Mutation(() => UserResponse),
+    __param(0, type_graphql_1.Arg("options", () => UsernamePasswordInput)),
+    __param(1, type_graphql_1.Ctx()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [UsernamePasswordInput, Object]),
+    __metadata("design:returntype", Promise)
+], UserResolver.prototype, "login", null);
 UserResolver = __decorate([
     type_graphql_1.Resolver()
 ], UserResolver);
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,6 @@
 import { MyContext } from "../types";
 import { User } from "../entities/User";
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
 import argon2 from 'argon2';
 
 
@@ -12,21 +12,115 @@ class UsernamePasswordInput {
     password: string
 }
 
+@ObjectType()
+class FieldError {
+    @Field()
+    field: string;
+
+    @Field()
+    message: string;
+}
+
+
+@ObjectType()
+class UserResponse {
+    // return user or errors if present
+    @Field(() => [FieldError], { nullable: true })
+    errors?: FieldError[]
+
+    @Field(() => User, { nullable: true })
+    user?: User
+}
+
 
 @Resolver()
 export class UserResolver {
-    @Mutation(() => User)
+    @Mutation(() => UserResponse)
     async register(
-        @Arg('options', () => UsernamePasswordInput) options: UsernamePasswordInput,
-        @Ctx() { em } : MyContext
+        // this is if type-graphQL doesnt infer, just put it like this
+        @Arg("options", () => UsernamePasswordInput)
+        options: UsernamePasswordInput,
+        @Ctx() { em }: MyContext
+    ): Promise<UserResponse> {
 
-    ) {
+        if (options.username.length <= 2) {
+            return {
+                errors: [{
+                    field: "username",
+                    message: "length must be greater than 2"
+                }]
+            }
+        }
+        if (options.username.length <= 2) {
+            return {
+                errors: [
+                    {
+                        field: "password",
+                        message: "length must be greater than 2",
+                    },
+                ],
+            };
+        }
+
         const hashPassword = await argon2.hash(options.password);
-        const user = em.create(User, { 
+        const user = em.create(User, {
             username: options.username,
-            password: hashPassword 
-        })
-        await em.persistAndFlush(user);
-        return user;
+            password: hashPassword,
+        });
+
+        try {
+            await em.persistAndFlush(user);
+        } catch(error) {
+            if (error.code === "23505" || error.detail.includes('already exists')) {
+                // duplicate username errpr
+                return {
+                    errors: [{
+                        field: 'username',
+                        message: 'username already taken'
+                    }]
+                }
+            }
+            console.log('message: ', error.message)
+        }
+
+        return {
+            user,
+        }
+    }
+
+    @Mutation(() => UserResponse)
+    async login(
+        // this is if type-graphQL doesnt infer, just put it like this
+        @Arg("options", () => UsernamePasswordInput)
+        options: UsernamePasswordInput,
+        @Ctx() { em }: MyContext
+    ): Promise<UserResponse> {
+        const user = await em.findOne(User, { username: options.username })
+        if (!user) {
+            return {
+                errors: [
+                    {
+                        field: "username",
+                        message: "username doesn't exist"
+                    },
+                ]
+            }
+        }
+        const valid = await argon2.verify(user.password, options.password);
+        if (!valid) {
+            return {
+                errors: [
+                    {
+                        field: "password",
+                        message: "incorrect password"
+                    },
+                ]
+            };
+        }
+        return {
+            user,
+        };
     }
 }
+
+
